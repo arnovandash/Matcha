59693891610ca06a483026de4abce252bc7a7741
Arno VAN WYK <arnovan-@e5r10p3.jnb.42.fr>
2016-11-01 14:16:31 +0200

    getting geolocation :)

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -13,6 +13,29 @@ app.controller('login__', function($http, $scope, $sessionStorage, partial) {
     };
 });
 
+app.controller('locate__', function($http, $scope, $sessionStorage, partial){
+
+    $scope.locate = function() {
+      console.log("Activating GPS...");
+    	navigator.geolocation.getCurrentPosition(on_success, on_error, {
+    		timeout: 5000});
+    };
+
+    function on_success(position){
+    	console.log("GPS activated sucessfully!");
+    	var lat = position.coords.latitude;
+    	var lng = position.coords.longitude;
+    	my_location = new google.maps.LatLng(lat, lng);
+      console.log(lat);
+      console.log(lng);
+    };
+
+    function on_error(error) {
+    	console.log("GPS activation failure!");
+    	//fallback on wifi and call tower location
+    };
+});
+
 app.config(function($mdThemingProvider) {
 	$mdThemingProvider.theme('default')
 		.primaryPalette('deep-purple');
