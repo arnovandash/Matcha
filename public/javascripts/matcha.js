47eb89aaf9d8e41ceff9067a33629c0228480631
angus <angusdippenaar@gmail.com>
2016-11-04 15:06:54 +0200

    made sruff work

--- public/javascripts/matcha.js
+++ public/javascripts/matcha.js
@@ -4,16 +4,16 @@ app.controller('matcha__', function($http, $scope, $location, $sessionStorage) {
 
 });
 
-/******************************************************************************/
-/*    This factory is just to simplify reloading partials that have the same  */
-/*    address, but different content rendered by the server for different     */
-/*    situations. For example: the home page says login or signup before the  */
-/*    user has logged in, but after they have logged in, it displays their    */
-/*    home page and logout information.                                       */
-/*                                                                            */
-/*    Include 'partial' as a dependency for controllers and use               */
-/*    'partial.reload()' to get the new page from the server                  */
-/******************************************************************************/
+/******************************************************************************
+ *    This factory is just to simplify reloading partials that have the same  *
+ *    address, but different content rendered by the server for different     *
+ *    situations. For example: the home page says login or signup before the  *
+ *    user has logged in, but after they have logged in, it displays their    *
+ *    home page and logout information.                                       *
+ *                                                                            *
+ *    Include 'partial' as a dependency for controllers and use               *
+ *    'partial.reload()' to get the new page from the server                  *
+ ******************************************************************************/
 app.factory('partial', ['$route', '$templateCache', function($route, $templateCache) {
 	return {
 		reload: function() {
