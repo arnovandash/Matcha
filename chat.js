9671452d00e4121eb1416de3b744be43b25d38fd
angus <angusdippenaar@gmail.com>
2016-11-13 10:24:42 +0200

    cleaned up a bit

--- chat.js
+++ chat.js
@@ -6,16 +6,15 @@ module.exports = {
 var mongo = require('./myMongo');
 
 function saveMessege(msg) {
-
-    msg.time = Math.round(new Date().getTime()/1000.0);
+    msg.time = Math.round(new Date().getTime() / 1000.0);
     mongo.insertChat(msg, function (result) {
         console.log(result);
     });
 }
 
 function getChat(to, from, callback) {
-    mongo.find('chat',{to:to, from:from},function (result) {
+    mongo.find('chat', {to:to, from:from}, function (result) {
         console.log(result);
         callback(result);
-    })
+    });
 }
